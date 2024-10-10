6be2cb97a2f4779e686ccbf2a8904f36204a5da1
lizeac <lizeacardoso@hotmail.com>
2024-10-10 19:04:53 -0300

    update django-on-heroku

--- excel_login/settings.py
+++ excel_login/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +27,7 @@ CSRF_COOKIE_SECURE=False
 SECRET_KEY = 'django-insecure-63o4*evf1r3h-f5l!jqa1z$bbkp@x5x_68nk7)^42)r*a!&n#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
